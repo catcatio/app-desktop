bc3223817d4440aeb9f206887f259e89abffe2f8
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2018-01-23 11:32:49 +0700

    refactor(exchange): Add cex for compare price

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const electron = require('electron')
 const { toCurrency } = require('./util')
+const fetcher = require('@rabbotio/fetcher')
+
 const app = electron.app
 let isInit = false
 
@@ -20,14 +22,35 @@ const getPrice = (pairs, from, to) => {
 }
 
 const fetchAndUpdate = async tray => {
-  const fetcher = require('@rabbotio/fetcher')
-  const pairs = await fetcher.getJSON('https://api.binance.com/api/v3/ticker/price').catch(() => {
-    tray.setTitle(`Hm!!`)
-  })
+  const pairs = await fetcher.getJSON('https://api.binance.com/api/v3/ticker/price').catch(console.error)
+  const { lprice } = await fetcher.getJSON(`https://cex.io/api/last_price/ETH/USD`).catch(console.error)
+
+  if (!pairs || !lprice) return tray.setTitle(`Hm!!`)
 
   const ETH_USDT = getTitle(pairs, 'ETH', 'USDT')
-  const XLM_ETH = toCurrency(getTitle(pairs, 'XLM', 'ETH').price * ETH_USDT.price)
-  tray.setTitle(`ETH $${ETH_USDT.title} | XLM $${XLM_ETH}`)
+  const XLM_ETH = toCurrency(getTitle(pairs, 'XLM', 'ETH').price, 6, 6)
+  const CEX_ETH_USD = toCurrency(lprice)
+  const CEX_DIFF = toCurrency(lprice - ETH_USDT.price)
+
+  // USD
+  tray.setTitle(`ETH ${CEX_ETH_USD} (${CEX_DIFF}) | XLM ${XLM_ETH}`)
+
+  // THB
+  // 932.91 -(33.124674405891244)-> 30,902.34
+  // 103.89 -(33.124651073250553)-> 3,441.32
+  // 34,343.66
+  // 0.747497229775266
+  // 1000.00 -> 0.74562100 -> 1337.7976 (44,314.109920982732084)
+  // Fee 0.001876229775266 = 0.25% = $2.5
+  // 0.74562100 ETH for 34,298.57 THB (46,000)
+  const USD_THB = 33.124674405891244
+  // 1036.53 -[33.114333400866352]-> 34,324
+  // 0.67857100 -[48500.99]-> 32829.08687208
+
+  const ETH_THB = toCurrency(ETH_USDT.price * USD_THB)
+  const CEX_ETH_THB = toCurrency(lprice * USD_THB)
+  const CEX_THB_DIFF = toCurrency((lprice - ETH_USDT.price) * USD_THB)
+  tray.setToolTip(`ETH ฿${ETH_THB}-${CEX_ETH_THB} (${CEX_THB_DIFF})`)
 }
 
 const createWindow = () => {
--- util.js
+++ util.js
@@ -1,5 +1,5 @@
-const toCurrency = value => {
-  return Number(value).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+const toCurrency = (value, maximumFractionDigits = 2, minimumFractionDigits = 2) => {
+  return Number(value).toLocaleString(undefined, { minimumFractionDigits, maximumFractionDigits })
 }
 
 module.exports = { toCurrency }
